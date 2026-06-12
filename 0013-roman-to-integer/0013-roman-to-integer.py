a2b1d9d628e567990d064b77c95273faed46f254
Anuvarshini2212 <anuvarshini2022@gmail.com>
2026-06-12 21:27:17 +0530

    Time: 7 ms (42.63%), Space: 19.3 MB (58.56%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -9,13 +9,10 @@ class Solution:
             "D": 500,
             "M": 1000
         }
-
         res = 0
-
         for i in range(len(s)):
             if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
                 res -= roman[s[i]]
             else:
                 res += roman[s[i]]
-
         return res
