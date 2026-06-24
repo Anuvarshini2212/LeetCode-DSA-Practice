cc3092619168286b7cde31449ed9d85d2998050a
Anuvarshini2212 <anuvarshini2022@gmail.com>
2026-06-24 16:18:14 +0530

    Time: 24 ms (69.01%), Space: 30.3 MB (35.92%) - LeetHub

---
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, nums: List[int], numsDivide: List[int]) -> int:
+        g=reduce(gcd, numsDivide)
+        nums.sort()
+        for i,num in enumerate(nums):
+            if g%num==0:
+                return i
+        return -1
+        
